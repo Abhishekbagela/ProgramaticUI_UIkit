e4ceda2524e15bff47e786073ec9f874d751d18c
Abhishek-ab@ios-dev <77952588+abhishekquantic@users.noreply.github.com>
2025-07-24 22:15:37 +0530

    Initial Commit

---
+++ ProgramaticUI_UIkit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ProgramaticUI_UIkit
+//
+//  Created by Abhishek Bagela on 24/07/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
